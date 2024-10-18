c5f698f4f781496646ad6357c4d2a805201b4c69
vandanachohan <161231376+vandanachohan@users.noreply.github.com>
2024-10-18 17:55:46 +0500

    fix error

--- src/app/components/Header/header.tsx
+++ src/app/components/Header/header.tsx
@@ -1,18 +1,13 @@
+
+
 import headerStyle from "./header.module.css";
 
-
-
-
-const Header = (props:any) =>{
-    console.log(props)
-    return (
-     <div>
-        <header className={headerStyle.header}><h1>My Next.js Website</h1>
-        </header> 
-    </div>
+const Header = () => {
+  return (
+    <header className={headerStyle.header}>
+      <h1>My Next.js Website</h1>
+    </header>
   );
-} ;
+};
 
-export default Header
-
-
+export default Header;
